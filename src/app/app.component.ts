825226f0025672a6f54ca753c04f5c2b00d293ff
simizu-masato <m.shimizu@trigger4.com>
2024-04-25 14:36:40 +0900

    タイムラインのモックデータを設置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '武井 検事', message: 'お疲れ様です。' },
+  { name: '武井 検事', message: 'この間の件はどうなりましたか？' },
+  { name: '清水 検事', message: 'お疲れ様です。' },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'chat-app-angular';
+  comments = COMMENTS;
 }
