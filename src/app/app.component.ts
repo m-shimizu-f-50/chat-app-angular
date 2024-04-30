c7d5c0113bdc2a1cd43318d18d89591278938e6d
simizu-masato <m.shimizu@trigger4.com>
2024-04-30 13:26:00 +0900

    不要コード削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,12 @@ import {
 const CURRENT_USER: User = new User(1, '武井 検事');
 const ANOTHER_USER: User = new User(2, '清水 検事');
 
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です。'),
-  new Comment(ANOTHER_USER, 'この間の件はどうなりましたか？'),
-  new Comment(CURRENT_USER, 'お疲れ様です。'),
-  new Comment(CURRENT_USER, 'クライアントからOKが出ました。'),
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // comments = COMMENTS;
   comments$: Observable<Comment[]>;
   commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
