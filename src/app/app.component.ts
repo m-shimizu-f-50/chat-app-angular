2f77031e5d4e362fa45f15efeae6d5667517a85c
simizu-masato <m.shimizu@trigger4.com>
2024-04-30 13:24:32 +0900

    チャット送信でFirebaseに追加処理実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/compat/database';
 
 const CURRENT_USER: User = new User(1, '武井 検事');
 const ANOTHER_USER: User = new User(2, '清水 検事');
@@ -21,18 +24,23 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  comments = COMMENTS;
+  // comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
   item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase) {
     this.item$ = this.db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.valueChanges();
   }
 
   addComment(comment: string): void {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.comment = '';
     }
   }
 }
